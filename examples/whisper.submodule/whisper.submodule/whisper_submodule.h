6449a426f2d51f31d7d2d7133def98c67cbad75d
Andriy Katkov <andriy.katkov@gmail.com>
2025-01-26 19:48:36 -0800

    add whisper.submodule for easier app integration

---
+++ examples/whisper.submodule/whisper.submodule/whisper_submodule.h
@@ -0,0 +1,18 @@
+//
+//  whisper_submodule.h
+//  whisper.submodule
+//
+//  Created by Andriy Katkov on 1/21/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for whisper_submodule.
+FOUNDATION_EXPORT double whisper_submoduleVersionNumber;
+
+//! Project version string for whisper_submodule.
+FOUNDATION_EXPORT const unsigned char whisper_submoduleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <whisper_submodule/PublicHeader.h>
+
+#import "whisper.h"
